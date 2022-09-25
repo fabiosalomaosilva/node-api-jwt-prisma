4cfc28e7ec4c0220e6efe51f6312b91cb22acb6c
root <root@FabioNote.localdomain>
2022-09-25 01:51:12 -0500

    Incluido o prisma e migrations

---
+++ src/Data/Dtos/RegisterRole.ts
@@ -0,0 +1,5 @@
+
+export interface RegisterRole {
+    id: string;
+    name?: string;
+}
---
+++ src/Data/Dtos/RegisterUser.ts
@@ -0,0 +1,9 @@
+export interface RegisterUser {
+    id: string;
+    name?: string;
+    lastName?: string;
+    email: string;
+    password: string;
+    photoUrl?: string;
+}
+
